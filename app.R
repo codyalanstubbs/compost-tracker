734c39f35f2a9aeffe6397fa43b4c55ab3a141a4
schtubbs <5488363+schtubbs@users.noreply.github.com>
2020-11-22 19:48:28 -0500

    First deploy! It works! YAY!

--- app.R
+++ app.R
@@ -1,9 +1,12 @@
 library(shiny)
-
+library(dplyr)
 # Define basic save and load data functions
 library(googlesheets4)
 
-table <- "https://docs.google.com/spreadsheets/d/1TFhO8xeayIuFCcGSDR8fhqGaD1N_QK122JLEzDJY8v4/edit?usp=sharing"
+table <- "https://docs.google.com/spreadsheets/d/15kufKnNNG2S18CvzNWUayN0QaG8J1ZJ9OD1tf0G203U/edit?usp=sharing"
+
+#drive_auth(cache = ".secrets") #for the first time running the app in R to get the OAuth token
+gs4_auth(cache = ".secrets", email = TRUE, use_oob = TRUE)
 
 saveData <- function(data) {
     # Add the data as a new row
@@ -15,83 +18,28 @@ loadData <- function() {
     read_sheet(table)
 }
 
-library(tidyverse)
-# make the breaks for the Count per Minute y-axis ----
-breaks <- c(10^(-10:10), 5*10^(-10:10))
-breaks <- as.data.frame( data.frame(breaks) %>% filter(breaks >= 1e-03, breaks <= 1e+03) )$breaks
-minor_breaks <- rep(1:9, 21)*(10^rep(-10:10, each=9))
-minor_breaks <- as.data.frame( data.frame(minor_breaks) %>% filter(minor_breaks >= 1e-03, minor_breaks <= 1e+03) )$minor_breaks
-
-# create key-value pairs for some handy conversions of the low count (bottom) portion of the y-axis ----
-min_key <- 
-    data.frame(
-        value = c(1, 0.5, 0.2, 0.1, 
-                  0.05, 0.02, 0.01, 
-                  0.005, 0.002, 0.001)) %>% 
-    mutate(key = 1/value)
-
-hrs_key <- 
-    data.frame(
-        value = c(15, 30, 60, 300,
-                  480, 960, 1440)) %>% 
-    mutate(key = value/60) %>% 
-    mutate(value = 1/value)
-
-x_values <- seq(0,150,7) # week duration values for the x-axis
-d <- data.frame() # blank dataframe
-
-# standard celeration chart using hrs_key ----
-scc <- ggplot(d, aes(x =0, y = 1/2000)) +
-    geom_point(alpha = 0) + # default point; remove 'alpha = 0' to make visible
-    #geom_point(pch = 4) + # x for errors
-    scale_y_log10(
-        name = "Behavior Count per Minute",
-        breaks = breaks, 
-        minor_breaks = minor_breaks, 
-        limits = c(1/2000,1e+03), 
-        labels = breaks, 
-        expand = c(0,0),
-        sec.axis = dup_axis(
-            name = "Hours to Complete 1 Behavior",
-            breaks = hrs_key$value,
-            labels = hrs_key$key) )+
-    scale_x_continuous(
-        name = "Successive Calendar Days",
-        limits = c(min(x_values), max(x_values)),
-        breaks = seq(min(x_values), max(x_values), 7),
-        minor_breaks = seq(min(x_values), max(x_values), 1), 
-        expand = c(0,1)) +
-    annotation_logticks(sides="l") +
-    theme_bw() +
-    theme(
-        panel.grid.major = element_line(color = "darkgrey"),    
-        panel.grid.minor = element_line(color = "grey"))
-
 # Shiny app with 3 fields that the user can submit data for
 shinyApp(
     ui = fluidPage(
-        DT::dataTableOutput("responses", width = 300), tags$hr(),
-        textInput("behavior.date", "Behavior Date (e.g.: 2020-11-17)", ""),
-        textInput("behaver", "Behaver", ""),
-        textInput("project", "Project", ""),
-        textInput("behavior", "Behavior", ""),
-        textInput("count", "Number of Behaviors Completed", ""),
-        textInput("duration", "Duration (Minutes)", ""),
+        textInput("initials", "Initials (e.g.: CAS):", ""),
+        textInput("item", "Item (e.g.: apple core, banana peel, egg shell, etc.", ""),
+        textInput("item.count", "Item Count (e.g.: if you weighed the egg shells of two eggs, then enter 2.)", ""),
+        textInput("weight", "Weight (grams)", ""),
         textInput("notes", "Notes", ""),
         actionButton("submit", "Submit"),
-        plotOutput("plot", width = "400px")
+        DT::dataTableOutput("responses", width = 300), tags$hr()
+        # ,
+        # plotOutput("plot", width = "400px")
     ),
     server = function(input, output, session) {
         
         # Whenever a field is filled, aggregate all form data
         formData <- reactive({
-            data <- data.frame(data.entry.date = Sys.Date(),
-                               behavior.date = input$behavior.date,
-                               behaver = input$behaver, 
-                               project = input$project, 
-                               behavior = input$behavior, 
-                               count = input$count, 
-                               duration = input$duration,
+            data <- data.frame(record.date = Sys.Date(),
+                               initials = input$initials, 
+                               item = input$item,
+                               item.count = input$item.count %>% as.numeric(.), 
+                               weight = input$weight %>% as.numeric(.),
                                notes = input$notes)
             
         })
@@ -107,7 +55,7 @@ shinyApp(
             input$submit
             loadData()
         })     
-        
-        output$plot <- renderPlot(scc, res = 100)
+         
+        # output$plot <- renderPlot(plot(x = formData$item, y = formData$weight), res = 100)
     }
 )
